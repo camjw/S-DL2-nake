a7cae8f2aaf632ebce4af3053197dfd0817782dd
camjw <camjw119@gmail.com>
2018-12-06 20:18:47 +0000

    Added lib files to main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
 #include "lib/display.h"
 
 int main( int argc, char* args[] ) {
+  Display game_display(480, 640);
+  game_display.run();
   return 0;
 }
