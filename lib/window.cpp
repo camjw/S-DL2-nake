35611be19b7d5a8664640aa9c0a6f5de26807fba
camjw <camjw119@gmail.com>
2018-12-09 21:37:38 +0000

    Added cpp file

---
+++ lib/window.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include "window.h"
+
+Window::Window(const std::string &title, int width, int height) {
+  _title = title; _width = width; _height = height;
+  _closed = !init();
+}
+
+Window::~Window() {
+  SDL_DestroyRenderer(_renderer);
+  SDL_DestroyWindow(_window);
+  SDL_Quit();
+}
+
+bool Window::init() {
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    std::cerr << "SDL failed to init" << std::endl;
+    return 0;
+  }
+
+  _window = SDL_CreateWindow(_title.c_str(), 100, 100, _width, _height, 0);
+
+  if(_window == nullptr) {
+    std::cerr << "SDL failed to create window" << std::endl;
+    return 0;
+  }
+
+  _renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_ACCELERATED);
+
+  if (_renderer == nullptr) {
+    std::cerr << "SDL failed to create renderer" << std::endl;
+    return 0;
+  }
+  return true;
+}
+
+void Window::pollEvents(SDL_Event &event) {
+  switch (event.type) {
+    case SDL_QUIT:
+      _closed = true;
+      break;
+    default:
+      break;
+  }
+}
+
+void Window::clear() const {
+  SDL_RenderPresent(_renderer);
+  SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 255);
+  SDL_RenderClear(_renderer);
+}
