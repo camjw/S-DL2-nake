efc1fb95f43f7fe733f54fe4cf70d6be1857b6ac
camjw <camjw119@gmail.com>
2018-12-09 21:38:04 +0000

    Added new files into main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,24 @@
 #include "lib/window.h"
 #include "lib/ship.h"
 
+void pollEvents(Window window, Ship ship) {
+  SDL_Event event;
+
+  if (SDL_PollEvent(&event)) {
+    ship.pollEvents(event);
+    window.pollEvents(event);
+  }
+}
 
 int main( int argc, char* args[] ) {
+  Window window("Asteroids", 800, 600);
+  Ship ship(window, 120, 120, 100, 100, 200, 0, 200, 255);
 
+  while (!window.isClosed()) {
+    pollEvents(window, ship);
+    ship.draw();
+    window.clear();
+  }
+
+  return 0;
 }
