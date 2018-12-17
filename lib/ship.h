41d5a13f5f9e5518777c67e6336e05004d45cce3
camjw <camjw119@gmail.com>
2018-12-17 20:51:13 +0000

    redfactored headers

--- lib/ship.h
+++ lib/ship.h
@@ -3,7 +3,6 @@
 #include "window.h"
 
 class Ship : public Window {
-
   public:
     Ship(const Window &window, int w, int h, int x, int y, int r, int g, int b, int a);
     void draw() const;
@@ -13,6 +12,7 @@ class Ship : public Window {
     int _w, _h;
     int _x, _y;
     int _r, _g, _b, _a;
+    float _theta = 0;
 };
 
 #endif
--- lib/window.h
+++ lib/window.h
@@ -4,25 +4,25 @@
 #include <SDL2/SDL.h>
 
 class Window {
-public:
-  Window(const std::string &title, int width, int height);
-  ~Window();
+  public:
+    Window(const std::string &title, int width, int height);
+    ~Window();
 
-  void pollEvents(SDL_Event &event);
-  void clear() const;
-  inline bool isClosed() const { return _closed; }
+    void pollEvents(SDL_Event &event);
+    void clear() const;
+    inline bool isClosed() const { return _closed; }
 
-private:
-  bool init();
-  std::string _title;
-  int _width = 800;
-  int _height = 600;
-  bool _closed = false;
+  private:
+    bool init();
+    std::string _title;
+    int _width = 800;
+    int _height = 600;
+    bool _closed = false;
 
-  SDL_Window *_window = nullptr;
+    SDL_Window *_window = nullptr;
 
-protected:
-  SDL_Renderer *_renderer = nullptr;
+  protected:
+    SDL_Renderer *_renderer = nullptr;
 };
 
 #endif
