6139ba591643e4c73816c6c5547787b69f8af052
camjw <camjw119@gmail.com>
2018-12-26 23:29:01 +0000

    added food tests

--- tests/food_test.cpp
+++ tests/food_test.cpp
@@ -1,13 +1,27 @@
 #include <vector>
 #include "food_test.h"
 
+using ::testing::AtLeast;
+using ::testing::ElementsAre;
+
 FoodTest::FoodTest() : mock_window(), subject(mock_window, 10, 10, 10, 10, 10, 10, 10, 0) {
 };
 
 TEST_F(FoodTest, LocationTest)
 {
-  std::vector<int> location = subject.getLocation();
-  ASSERT_TRUE(location.at(0) == 0);
-  ASSERT_TRUE(location.at(1) == 10);
+  EXPECT_THAT(subject.getLocation(), ElementsAre(0, 10));
+}
 
+TEST_F(FoodTest, DrawTest)
+{
+  subject.draw();
+  EXPECT_CALL(mock_window, renderRect(subject.getLocation(), 1, 1, 1, 1, 1)).Times(AtLeast(1));
+}
+
+TEST_F(FoodTest, ResetLocationTest)
+{
+  std::deque<std::vector<int>> fakeLocationHistory;
+  fakeLocationHistory.push_back(std::vector<int> {0, 0});
+  subject.resetLocation(fakeLocationHistory);
+  EXPECT_THAT(subject.getLocation(), ElementsAre(50, 20));
 }
