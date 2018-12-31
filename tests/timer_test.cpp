9f22543e0f13220f3d4e82b285f315ea0a9e08d6
camjw <camjw119@gmail.com>
2018-12-31 17:15:12 +0000

    Added more timer tests

--- tests/timer_test.cpp
+++ tests/timer_test.cpp
@@ -8,7 +8,6 @@ void TimerTest::TearDown() {
   delete pTimer_;
 }
 
-
 TEST_F(TimerTest, StartedTest)
 {
   pTimer_->start();
@@ -20,4 +19,33 @@ TEST_F(TimerTest, PausedTest)
   pTimer_->start();
   pTimer_->pause();
   EXPECT_TRUE(pTimer_->isPaused());
+}
+
+TEST_F(TimerTest, StoppedTest)
+{
+  pTimer_->start();
+  pTimer_->stop();
+  EXPECT_TRUE(!pTimer_->isStarted());
+}
+
+TEST_F(TimerTest, StoppedPausedTest)
+{
+  pTimer_->start();
+  pTimer_->pause();
+  pTimer_->stop();
+  EXPECT_TRUE(!pTimer_->isPaused());
+}
+
+TEST_F(TimerTest, UnpausedTest)
+{
+  pTimer_->start();
+  pTimer_->pause();
+  pTimer_->unpause();
+  EXPECT_TRUE(!pTimer_->isPaused());
+}
+
+TEST_F(TimerTest, TicksTest)
+{
+  pTimer_->start();
+  EXPECT_EQ(pTimer_->getTicks(), 0);
 }
