0da48f61eeca6f0258dcc2c465bea498925e157a
camjw <camjw119@gmail.com>
2019-01-02 14:54:43 +0000

    main uses background class too

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <SDL2/SDL_image.h>
 #include <iostream>
 #include "lib/renderer.h"
+#include "lib/background.h"
 #include "lib/snake.h"
 #include "lib/food.h"
 #include "lib/timer.h"
@@ -16,8 +17,9 @@ int main( int argc, char* args[] ) {
   Renderer renderer("Snake", GRID_WIDTH, GRID_HEIGHT, GRID_STRIDE);
   Snake snake(renderer, GRID_STRIDE, 20, 20, 255, 255, 255, 255);
   Food food(renderer, GRID_HEIGHT, GRID_HEIGHT, GRID_STRIDE, 192, 192, 192, 255, time(0));
+  Background background(renderer, GRID_WIDTH, GRID_HEIGHT, GRID_STRIDE);
   Timer fpsTimer;
   Timer capTimer;
-  Game game(&renderer, &snake, &food, &fpsTimer, &capTimer, GRID_WIDTH, GRID_HEIGHT, GRID_STRIDE, SCREEN_FPS);
+  Game game(&renderer, &background, &snake, &food, &fpsTimer, &capTimer, GRID_WIDTH, GRID_HEIGHT, GRID_STRIDE, SCREEN_FPS);
   game.run();
 }
