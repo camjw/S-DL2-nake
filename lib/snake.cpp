3b9b73bf3e7409e8e7d8d902bf6b48fab459b206
camjw <camjw119@gmail.com>
2019-01-03 20:36:03 +0000

    snake location randomly generated, directions gives user than best chance

--- lib/snake.cpp
+++ lib/snake.cpp
@@ -1,17 +1,44 @@
 #include "snake.h"
 #include <iostream>
 
-Snake::Snake(const Renderer &renderer, int stride, int x, int y, int r, int g, int b, int a) :
-  Renderer(renderer), _stride(stride), _x(x), _y(y), _r(r), _g(g), _b(b), _a(a), _dx(0), _dy(0)
+Snake::Snake(const Renderer &renderer, int grid_size, int stride, int r, int g, int b, int a, int random_seed) :
+Renderer(renderer), _grid_stride(stride), _r(r), _g(g), _b(b), _a(a), _dx(0), _dy(0), _grid_size(grid_size)
 {
+  srand(random_seed);
+  initialiseLocation();
 }
 
 void Snake::draw() {
   move();
   for (int i = 0; i < locationHistory.size(); ++i) {
-    renderRect(locationHistory.at(i), _stride, _r, _g, _b, _a);
+    renderRect(locationHistory.at(i), _grid_stride, _r, _g, _b, _a);
   }
   acceptingMove = true;
+}
+
+void Snake::initialiseLocation() {
+  _x = ((rand() % (_grid_size - 2)) + 1) * _grid_stride;
+  _y = ((rand() % (_grid_size - 2)) + 1) * _grid_stride;
+  int x_offset = abs(_x - (_grid_size * _grid_stride) /2);
+  int y_offset = abs(_y - (_grid_size * _grid_stride) /2);
+  setDirection(x_offset, y_offset);
+}
+
+void Snake::setDirection(int x_offset, int y_offset) {
+  if (x_offset > y_offset) {
+    if (_x - (_grid_size * _grid_stride) /2 > 0) {
+      currentDir = LEFT;
+    } else {
+      currentDir = RIGHT;
+    }
+  } else {
+    if (_y - (_grid_size * _grid_stride) /2 > 0) {
+      currentDir = UP;
+    } else {
+      currentDir = DOWN;
+    }
+  }
+  attemptedDir = currentDir;
 }
 
 void Snake::updateLocationHistory() {
@@ -23,20 +50,20 @@ void Snake::updateLocationHistory() {
 
 void Snake::updateDirection() {
   if (attemptedDir == LEFT && currentDir != RIGHT) {
-    _dx = -_stride;
+    _dx = -_grid_stride;
     _dy = 0;
     currentDir = LEFT;
   } else if (attemptedDir == RIGHT && currentDir != LEFT) {
-    _dx = _stride;
+    _dx = _grid_stride;
     _dy = 0;
     currentDir = RIGHT;
   } else if (attemptedDir == UP && currentDir != DOWN) {
     _dx = 0;
-    _dy = -_stride;
+    _dy = -_grid_stride;
     currentDir = UP;
   } else if (attemptedDir == DOWN && currentDir != UP) {
     _dx = 0;
-    _dy = _stride;
+    _dy = _grid_stride;
     currentDir = DOWN;
   }
   acceptingMove = false;
@@ -96,9 +123,9 @@ std::vector<int> Snake::checkSelfEat() {
 
 void Snake::checkHitWall(int grid_height) {
   std::vector<int> headSquare = locationHistory[locationHistory.size() -1];
-  if (headSquare.at(0) >= (grid_height - 1) * _stride || headSquare.at(0) <= 0) {
+  if (headSquare.at(0) >= (grid_height - 1) * _grid_stride || headSquare.at(0) <= 0) {
     alive = false;
-  } else if (headSquare.at(1) >= (grid_height - 1) * _stride || headSquare.at(1) <= 0) {
+  } else if (headSquare.at(1) >= (grid_height - 1) * _grid_stride || headSquare.at(1) <= 0) {
     alive = false;
   }
 }
@@ -113,19 +140,16 @@ bool Snake::isDead() {
 
 void Snake::showDeath() {
   for (int i = 0; i < locationHistory.size() - 1; ++i) {
-    renderRect(locationHistory.at(i), _stride, _r, _g, _b, _a);
+    renderRect(locationHistory.at(i), _grid_stride, _r, _g, _b, _a);
   }
   std::vector<int> deathSquare = locationHistory[locationHistory.size() -1];
-  renderRect(deathSquare, _stride, 255, 0, 0, 255);
+  renderRect(deathSquare, _grid_stride, 255, 0, 0, 255);
 }
 
 void Snake::reset() {
-  _x = 20;
-  _y = 20;
+  initialiseLocation();
   _currentLength = 4;
   alive = true;
-  currentDir = RIGHT;
-  attemptedDir = RIGHT;
   while (locationHistory.size() > 0) {
     locationHistory.pop_front();
   }
--- lib/snake.h
+++ lib/snake.h
@@ -3,11 +3,12 @@
 #include <vector>
 #include <deque>
 #include <algorithm>
+#include <cstdlib>
 #include "renderer.h"
 
 class Snake : public Renderer {
   public:
-    Snake(const Renderer &renderer, int stride, int x, int y, int r, int g, int b, int a);
+    Snake(const Renderer &renderer, int grid_size, int stride, int r, int g, int b, int a, int random_seed);
     virtual void draw();
     void move();
     void pollEvents(SDL_Event &event);
@@ -24,6 +25,8 @@ class Snake : public Renderer {
   private:
     void updateLocationHistory();
     void updateDirection();
+    void initialiseLocation();
+    void setDirection(int x_offset, int y_offset);
     std::vector<int> checkSelfEat();
     void checkHitWall(int grid_width);
     std::deque<std::vector<int>> locationHistory;
@@ -32,10 +35,10 @@ class Snake : public Renderer {
     bool acceptingMove = true;
     bool alive = true;
     int _currentLength = 4;
-    int _stride;
     int _x, _y;
     int _dx, _dy;
     int _r, _g, _b, _a;
+    int _grid_size, _grid_stride;
 };
 
 #endif
